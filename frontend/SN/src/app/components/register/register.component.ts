a39eb6e75cb7ae2fde8831f9d53db16e066d4176
1269124 <99384668+1269124@users.noreply.github.com>
2022-05-20 17:20:18 +0300

    register v?
    I think I'm making progress but I still have no fucking clue how to access the data from the form, I'm gonna watch the lectures tmr to see if there isn't anything about that

--- frontend/SN/src/app/components/register/register.component.ts
+++ frontend/SN/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthguardService} from "../../services/authguard.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,22 +9,35 @@ import {AuthguardService} from "../../services/authguard.service";
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl('', Validators.required)
+});
 
   constructor(private router: Router,
-              private authService: AuthguardService) { }
+              private authService: AuthguardService,
+              private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', Validators.required]
+    });
   }
+
+  /*get f() { return this.registerForm.controls; }*/
 
   registerUser(){
 
-
+    console.log(JSON.stringify(this.registerForm.value));
 
     this.router.navigateByUrl("/login")
   }
 
   onSubmit(){
-    
+    this.router.navigateByUrl("/login")
   }
 
   return(){
