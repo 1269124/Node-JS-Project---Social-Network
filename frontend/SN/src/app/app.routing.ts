e04830cb7026f5ab5c43b53c418593dcf2f5ff89
1269124 <99384668+1269124@users.noreply.github.com>
2022-06-01 22:15:33 +0300

    frontend before clean up
    frontend parts put together without cleaning up the project, still missing connection between frontend and backend

---
+++ frontend/SN/src/app/app.routing.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LandingComponent } from './examples/landing/landing.component';
+import { LoginComponent } from './examples/login/login.component';
+import { ProfileComponent } from './examples/profile/profile.component';
+
+const routes: Routes =[
+    { path: '', redirectTo: 'examples/login', pathMatch: 'full' },
+    { path: 'examples/landing',     component: LandingComponent },
+    { path: 'examples/login',       component: LoginComponent },
+    { path: 'examples/profile',     component: ProfileComponent }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+    ],
+})
+export class AppRoutingModule { }
